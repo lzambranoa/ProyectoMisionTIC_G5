54234292a069b90a7d8983f4d861711c413055e5
lzambranoa <lzambranoa.software@gmail.com>
2021-10-07 16:26:56 -0500

    se crea la validacion del input nombre

--- form4.js
+++ form4.js
@@ -1,8 +1,24 @@
-function validar_nombre(){
+
+function validar_nombre(nombre) {
+    let nombreValido = nombre.replace(/ /g, "");
+    let validator = /\d|\W/;
+    let resultado = validator.test(nombreValido);
+    let msm = document.getElementById('error');
+
+    if (nombre.length > 4 && nombre.length <= 30 && !resultado) {
+        let elemento = document.getElementById('campoNombre');
+        elemento.className = 'controls'
+        msm.innerText = "";
+    } else {
+        let elemento = document.getElementById('campoNombre');
+        elemento.className = 'error'
+        msm.innerText = "El nombre debe contener entre 4 y 30 caracteres, solo se aceptan letras";
+    }
+
 
 }
 
-function validar_contrasenas(){
+function validar_contrasenas() {
 
 }
 
