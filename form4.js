87297718fca65d5d1d374470fc66448fb385f423
lzambranoa <lzambranoa.software@gmail.com>
2021-10-09 08:59:07 -0500

    Comentarios para sprint

--- form4.js
+++ form4.js
@@ -1,35 +1,37 @@
 
 function validar_nombre(nombre) {
-    let nombreValido = nombre.replace(/ /g, "");
     let validator = /\d|\W/;
+    let nombreValido = nombre.replace(/ /g, "");
     let resultado = validator.test(nombreValido);
-    let msm = document.getElementById('error');
+    //let msm = document.getElementById('error');
 
     if (nombre.length > 4 && nombre.length <= 30 && !resultado) {
-        let elemento = document.getElementById('campoNombre');
-        elemento.className = 'controls'
-        msm.innerText = "";
+        // let elemento = document.getElementById('campoNombre');
+        // elemento.className = 'controls'
+        // msm.innerText = "";
+        return true;
     } else {
-        let elemento = document.getElementById('campoNombre');
-        elemento.className = 'error'
-        msm.innerText = "El nombre debe contener entre 4 y 30 caracteres, solo se aceptan letras";
+        // let elemento = document.getElementById('campoNombre');
+        // elemento.className = 'error'
+        // msm.innerText = "El nombre debe contener entre 4 y 30 caracteres, solo se aceptan letras";
+        return false;
     }
 
 
 }
 
-function validar_contrasenas() {
-    var p1 = document.getElementById("campoPassword").value;
-    var p2 = document.getElementById("campoConfirmarPassword").value;
+function validar_contrasenas(p1, p2) {
+    // var p1 = document.getElementById("campoPassword").value;
+    // var p2 = document.getElementById("campoConfirmarPassword").value;
     var espacios = false;
     var cont = 0;
-  
+    
     while (!espacios && (cont < p1.length)) {
             if (p1.charAt(cont) == " ")
                 espacios = true;
                 cont++;            
         }
-   
+
     if (espacios) {
     alert ("La contraseña no puede contener espacios en blanco");
     return false;
