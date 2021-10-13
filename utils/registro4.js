efc026eba46fc720549acc684c3513e0efad50ff
lzambranoa <lzambranoa.software@gmail.com>
2021-10-13 16:48:16 -0500

    Primer envio del sprint3

--- utils/registro4.js
+++ utils/registro4.js
@@ -1,13 +1,40 @@
-function recibirEdad(){
-
+function recibirEdad() {
+    let edad;
+    // edad = prompt("Ingrese su edad");
+    // console.log(edad);
+    return edad;
 }
 
-function ordenarEdad(args){
 
+function ordenarEdad(edad) {
+    let items = [];
+    let objeto = {
+        'nombre': document.getElementById('campoNombre').value,
+        'contrasena': document.getElementById('campoContrasena').value,
+        'correo': document.getElementById('campoCorreo').value,
+        'confirmacioncontrasena': document.getElementById('campoconfirmarContrasena').value,
+        'telefono': document.getElementById('campoTelefono').value,
+        'edad': edad
+    }
+
+    if (items.length < 20) {
+        items.push(objeto);
+    }
+
+    items.sort((a, b) => b.edad - a.edad);
+    //console.log(items);
+    return items;
 }
 
-function promedioEdad(args){
+function promedioEdad(args) {
+    let contador = 0;
+    for (var keys in args) {
+        contador += args[keys]['edad']
+    }
 
+    let promedio = contador / args.length;
+
+    return promedio;
 }
 
 module.exports.recibirEdad = recibirEdad;
